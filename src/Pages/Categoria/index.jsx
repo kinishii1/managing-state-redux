a2ae55ff75510f451643d85e44c24e329aa24e81
Kin Ishii <kinishii97@gmail.com>
2023-10-06 07:42:36 +0900

    terminando categorias

--- src/Pages/Categoria/index.jsx
+++ src/Pages/Categoria/index.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import Header from '../../components/Header'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
+import React from "react";
+import Header from "../../components/Header";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Categoria() {
-  const { id } = useParams()
-  const categoria = useSelector(state => state.categorias.find(categoria => categoria.id === id))
+  const { id } = useParams();
+  const categoria = useSelector((state) =>
+    state.categorias.find((categoria) => categoria.id === id)
+    );
+  const itens = useSelector((state) => state.itens.filter((item) => item.categoria === id));
+
+  console.log(itens);
   return (
     <div>
-      <Header 
-      titulo={categoria.nome}
-      descricao={categoria.descricao}
-      imagem={"assets/inicial.png"}
+      <Header
+        titulo={categoria.nome}
+        descricao={categoria.descricao}
+        imagem={"assets/inicial.png"}
       />
+       {/* <div>
+        {itens?.map(item => (
+          // <Item key={item.id} {...item} />
+        ))}
+        </div> */}
     </div>
-  )
+  );
 }
 
-export default Categoria
+export default Categoria;
